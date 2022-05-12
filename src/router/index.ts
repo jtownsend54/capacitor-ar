ab26fc4b31f02250cea49525786a86f81f52f74c
Jon Townsend <jon.townsend@limelyte.com>
2022-05-12 08:08:57 -0700

    Initial commit

---
+++ src/router/index.ts
@@ -0,0 +1,28 @@
+import { createRouter, createWebHistory } from '@ionic/vue-router';
+import { RouteRecordRaw } from 'vue-router';
+import HomePage from '../views/HomePage.vue'
+import ARPage from '@/views/ARPage.vue';
+
+const routes: Array<RouteRecordRaw> = [
+  {
+    path: '/',
+    redirect: '/home'
+  },
+  {
+    path: '/home',
+    name: 'Home',
+    component: HomePage
+  },
+  {
+    path: '/ar',
+    name: 'AR',
+    component: ARPage
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ src/views/ARPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <ion-page>
+
+  </ion-page>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import { IonPage} from '@ionic/vue';
+import { CapacitorARKit } from 'capacitor-arkit';
+
+export default defineComponent({
+  name: 'ARPage',
+  components: {
+    IonPage
+  },
+  ionViewDidEnter() {
+    CapacitorARKit.echo({'value': "test"});
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
